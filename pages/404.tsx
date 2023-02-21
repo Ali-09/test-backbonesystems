7049f67487e335d77d4c7a89c9bee0effd3c26da
Ali-09 <yuzet.ali@gmail.com>
2023-02-21 01:06:33 -0600

    fix: name in component page404, change page404 to Page404

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,9 +3,9 @@ import { useRouter } from "next/router";
 import { Box, Button, Container, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 
-export default function page404() {
+export default function Page404() {
   
-  const router = useRouter()
+  const router = useRouter();
 
   return (
     <Box
@@ -26,7 +26,7 @@ export default function page404() {
             <Typography variant="h5" align="center">
               La pagina que estas buscando no existe
             </Typography>
-            <Button sx={{marginTop: "2rem"}} variant="contained" onClick={()=>router.push("/")}>Regresar</Button>
+            <Button sx={{marginTop: "2rem"}} variant="contained" onClick={() => router.push("/")}>Regresar</Button>
           </Grid>
         </Grid>
       </Container>
