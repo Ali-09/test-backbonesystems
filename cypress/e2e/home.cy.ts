f6b6760216717c91f5df135abfe3980e691c07f5
Ali-09 <yuzet.ali@gmail.com>
2023-02-21 21:34:15 -0600

    dd cypress in proyect and create first test for page home

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+describe('/', () => {
+  it('Render table contacts', () => {
+    cy.visit('/');
+    cy.get('.MuiTable-root')
+      .find('.MuiTableCell-root')
+      .invoke('text');
+  });
+  it('Search in table contacts', () => {
+    cy.visit('/');
+    cy.get('input[placeholder="Busqueda por correo"]')
+      .type('a');
+  });
+  it('Click button Nuevo contacto', () => {
+    cy.visit('/');
+    cy.get('button')
+      .contains('Nuevo contacto')
+      .click()
+      .url()
+      .get('h4')
+      .contains('Nuevo contacto');
+  });
+  it('Click button Editar contacto', () => {
+    cy.visit('/');
+    cy.get('.MuiTableRow-root [data-testid="EditIcon"] > path')
+      .first()
+      .click()
+      .url()
+      .get('h4')
+      .contains('Modificar contacto');
+  });
+  it('Click button Eliminar contacto', () => {
+    cy.visit('/');
+    cy.get('.MuiTableRow-root [data-testid="DeleteIcon"] > path')
+      .first()
+      .click()
+      .url()
+      .get('h4')
+      .contains('Eliminar contacto');
+  });
+});
+
+export {};
